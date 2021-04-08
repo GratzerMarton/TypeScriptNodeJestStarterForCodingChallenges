55341b1db3308cf5932bb6bd656cbbe382d37456
GratzerMarton <gratzer.marton@gmail.com>
2021-04-08 16:57:00 +0200

    refactored for better readability

--- app.ts
+++ app.ts
@@ -1,3 +1,3 @@
-import { multiplyByThree } from "./components/multiplier/multiplier"
+import { multiplyByThree } from "./components/multiplier"
 
 console.log(multiplyByThree(3))
--- components/multiplier/index.test.js
+++ components/multiplier/index.test.js
@@ -1,4 +1,4 @@
-import { multiplyByThree } from "./multiplier.ts";
+import { multiplyByThree } from "./index.ts";
 
 test("should 2 * 3 equal to 6", () => {
   expect(multiplyByThree(3)).toBe(9);
